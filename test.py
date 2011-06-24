41c3dddf7f274ddb6e308052b942c39bef3dce9a
Tim Ekl <lithium3141@gmail.com>
2011-06-24 14:25:13 -0600

    Test for remove()
    Signed-off-by: Tim Ekl <lithium3141@gmail.com>

--- test.py
+++ test.py
@@ -24,5 +24,23 @@ class TestInsert(unittest.TestCase):
         self.root.insert(6)
         self.assertEqual(str(self.root), '1345689')
 
+class TestRemove(unittest.TestCase):
+   
+    def setUp(self):
+        self.root = TreeNode(5)
+        self.root.insert(3)
+        self.root.insert(7)
+        self.root.insert(1)
+        self.root.insert(4)
+        self.root.insert(6)
+        self.root.insert(9)
+        self.root.insert(2)
+        self.root.insert(8)
+        self.assertEqual(str(self.root), '123456789')
+
+    def test_one_remove(self):
+        self.root.remove(3)
+        self.assertEqual(str(self.root), '12456789')
+
 if __name__ == '__main__':
     unittest.main()
